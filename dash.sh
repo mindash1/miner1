ab8f443a6ba6bb1ea7e34fc6078566f1d4f3ddbd
mindash1 <96517740+mindash1@users.noreply.github.com>
2021-12-22 14:35:37 +0700

    Create dash.sh

---
+++ dash.sh
@@ -0,0 +1,4 @@
+sudo apt update
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+./cpuminer-sse2 -a yespowerURX -o stratum+tcp://yespowerURX.eu.mine.zpool.ca:6236 -u Xhf3vVC9Wu2NSdYRH1RwJjiZcbx7qCgrrj -p c=DASH -t 4
